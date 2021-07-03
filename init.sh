933373dbadb4a199d64d5ba325116ef4612602ef
Alex Malykh <alexeyheather@gmail.com>
2021-07-03 23:22:57 +0700

    now its possible to specify folder where project will be created via getopts

--- init.sh
+++ init.sh
@@ -1,17 +1,52 @@
-#!/bin/sh
+#!/bin/bash
 
-projectinit() {
-    PROJECTNAME=$1
+usage() {
+    cat <<EOF
+Project initializer script:
+
+OPTIONS:
+   -n      project name
+   -e      code editor to open newly created project in
+   -f      folder where to create a new project
+EOF
+}
+
+init() {
+    NEW_PROJECT_FOLDER=$PROJECTSDIR
+
+    # exit if no values passed to flag
+    if [ $# -eq 0 ]; then
+        usage
+        exit 0
+    fi
+
+    # setup flags
+    while getopts ":n:e:f:h" flag; do
+        case "${flag}" in
+        n) PROJECTNAME="${OPTARG}" ;;
+        e) CODE_EDITOR=${OPTARG} ;;
+        f) NEW_PROJECT_FOLDER="${OPTARG}" ;;
+        h) 
+            usage 
+            exit 0
+            ;;
+        *)
+            usage
+            exit 0
+            ;;
+        esac
+    done
+    shift $((OPTIND - 1))
 
     # cd to project initializer dir to source env variables
     cd $PROJECTSDIR/project-initializer
 
     # Check if python script executed without errors
-    if python3 . $PROJECTNAME; then
+    if python3 . $PROJECTNAME $NEW_PROJECT_FOLDER; then
         # Move to created project folder
-        cd $PROJECTSDIR/$PROJECTNAME
+        cd $NEW_PROJECT_FOLDER/$PROJECTNAME
 
-        # GitHub quick setup
+        # GitHub initialize new repo
         echo "# $PROJECTNAME" >> README.md
         git init
         git add README.md
@@ -20,9 +55,9 @@ projectinit() {
         git remote add origin git@github.com:$GITHUB_LOGIN/$PROJECTNAME.git
         git push -u origin main
 
-        # Open code editor in created project folder
-        if [ ! -z "$2" ]; then
-            $2 .
+        # Open created project folder in editor
+        if [ ! -z "$CODE_EDITOR" ]; then
+            $CODE_EDITOR .
         fi
 
         echo "success"
